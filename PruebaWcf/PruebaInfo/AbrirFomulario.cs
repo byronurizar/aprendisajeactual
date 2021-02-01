f37e73a32004d2a99852fb68ccc6f6f2031ef435
Byron Lopez <urizarcode@gmail.com>
2021-01-31 19:45:49 -0600

    Pruebas para wcf

---
+++ PruebaWcf/PruebaInfo/AbrirFomulario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaInfo
+{
+   public class AbrirFomulario
+    {
+      public string  open()
+        {
+            Form1 form = new Form1();
+            if (form.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+            return form.resultado;
+            }
+            else
+            {
+                return "El formulario se cerro por el usuario";
+            }
+            
+        }
+        
+    }
+}
---
+++ PruebaWcf/PruebaInfo/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace PruebaInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(142, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(193, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Enviar DAta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(126, 58);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(415, 38);
+            this.txtData.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button2.Location = new System.Drawing.Point(341, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(193, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtData);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtData;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
