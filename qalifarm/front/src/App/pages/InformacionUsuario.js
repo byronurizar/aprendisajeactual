58eff0e9670b588fbbb07227b2791be9f6d89d13
Byron Home <blu.urizar@gmail.com>
2021-04-21 23:02:00 -0600

    Se elimino proyecto

--- qalifarm/front/src/App/pages/InformacionUsuario.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Infouser } from '../components/base/Infouser';
-const InformacionUsuario = () => {
-    return (
-      <Infouser/>
-    )
-}
-
-export default InformacionUsuario;
--- qalifarm/front/src/App/pages/PersonaPage.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react'
-import { PersonaListar } from '../components/base/PersonaListar';
-const PersonaPage = () => {
-    return (
-        <PersonaListar />
-    )
-}
-
-export default PersonaPage;
